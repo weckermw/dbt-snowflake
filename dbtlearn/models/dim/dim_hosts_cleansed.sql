54d17f07c541e53cfa2cb21bf52730f43fbcc66b
Michael Wecker <michael.wecker@capgemini.com>
2024-10-03 15:05:40 +0200

    fixed spelling error

--- dbtlearn/models/dim/dim_hosts_cleansed.sql
+++ dbtlearn/models/dim/dim_hosts_cleansed.sql
@@ -2,14 +2,21 @@
   config(
     materialized = 'view'
     )
-}}
-WITH src_hosts AS(
-    SELECT * FROM {{ref("src_hosts")}}
+}} 
+WITH src_hosts AS (
+    SELECT
+        *
+    FROM
+        {{ ref('src_hosts') }}
 )
 SELECT
     host_id,
-    NVL(host_name, 'Anonymous') AS host_name,
-    IS_SUPERHOST,
-    CREATED_AT,
-    UPDATED_AT
-FROM src_hosts
+    NVL(
+        host_name,
+        'Anonymous'
+    ) AS host_name,
+    is_superhost,
+    created_at,
+    updated_at
+FROM
+    src_hosts
--- dbtlearn/models/src/src_hosts.sql
+++ dbtlearn/models/src/src_hosts.sql
@@ -1,5 +1,5 @@
 WITH raw_hosts AS(
-    SELECT * FROM  {{ source('airbnb', 'reviews')}}
+    SELECT * FROM  {{ source('airbnb', 'hosts')}}
 )
 SELECT
     ID AS host_id,
