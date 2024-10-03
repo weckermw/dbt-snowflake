3118e865a6faa270e9a663a8136ea353b5df99cb
Michael Wecker <michael.wecker@capgemini.com>
2024-10-03 11:33:54 +0200

    updated select statement in fct_reviews

--- dbtlearn/models/src/src_reviews.sql
+++ dbtlearn/models/src/src_reviews.sql
@@ -2,7 +2,9 @@ WITH raw_reviews AS(
     SELECT * FROM AIRBNB.RAW.RAW_REVIEWS
 )
 SELECT 
+    listing_id,
     date AS review_date,
+    reviewer_name,
     comments AS review_text,         
     sentiment AS review_sentiment
 FROM
