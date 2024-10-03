18887a0f0413a9f5381d77298c5558da90be63ce
Michael Wecker <michael.wecker@capgemini.com>
2024-10-03 11:54:09 +0200

    added straggler files to last commit 1 second ago

---
+++ dbtlearn/models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,15 @@
+{{
+  config(
+    materialized = 'view'
+    )
+}}
+WITH src_hosts AS(
+    SELECT * FROM {{ref("src_hosts")}}
+)
+SELECT
+    host_id,
+    NVL(host_name, 'Anonymous') AS host_name,
+    IS_SUPERHOST,
+    CREATED_AT,
+    UPDATED_AT
+FROM src_hosts
