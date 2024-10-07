219d212676cdcb4d5315d47d4faf70d9838af947
Michael Wecker <michael.wecker@capgemini.com>
2024-10-07 10:10:14 +0200

    updated pip

--- dbtlearn/models/fct/fct_reviews.sql
+++ dbtlearn/models/fct/fct_reviews.sql
@@ -14,5 +14,12 @@ SELECT
 FROM src_reviews
 WHERE review_text is not null
 {% if is_incremental() %}
+  {% if var("start_date", False) and var("end_date", False) %}
+    {{ log('Loading ' ~ this ~ ' incrementally (start_date: ' ~ var("start_date") ~ ', end_date: ' ~ var("end_date") ~ ')', info=True) }}
+    AND review_date >= '{{ var("start_date") }}'
+    AND review_date < '{{ var("end_date") }}'
+  {% else %}
     AND review_date > (select max(review_date) from {{ this }})
+    {{ log('Loading ' ~ this ~ ' incrementally (all missing dates)', info=True)}}
+  {% endif %}
 {% endif %}
